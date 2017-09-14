8bac7c827112b1f9a053eff970ff562fe7dda5e7
Fotis E. Psomopoulos <fpsom@issel.ee.auth.gr>
2017-09-14 14:54:05 +0300

    Second try for rst in md

--- conf.py
+++ conf.py
@@ -279,3 +279,14 @@ from recommonmark.parser import CommonMarkParser
 source_parsers = {
     '.md': CommonMarkParser,
 }
+
+github_doc_root = 'https://github.com/fpsom/reproducible-analysis-workshop'
+# app setup hook
+def setup(app):
+    app.add_config_value('recommonmark_config', {
+        'url_resolver': lambda url: github_doc_root + url,
+        'auto_toc_tree_section': 'Contents',
+        'enable_eval_rst': True,
+        'enable_auto_doc_ref': True,
+    }, True)
+    app.add_transform(AutoStructify)
